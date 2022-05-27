1ffd9f0179b1d89a2a984e36001e24d49589590c
Liam Eller <liamthelastson@gmail.com>
2022-05-26 17:43:54 -0700

    Add new mock data and change logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <>
       <Header/>
-      <h1 className="text-center position-absolute" style={{top: "50%", right: "50%",transform: "translate(50%, 50%)"}}>Incredable 3D interactive display and CSS animations. <span className="font-size-1">Are you inpressed yet?</span></h1>
+      <h1 className="text-center position-absolute" style={{top: "40%", right: "50%",transform: "translate(50%, 50%)"}}>Incredable 3D interactive display using React-Three-Fiber and ThreeJS.</h1>
     </>
   )
 }
